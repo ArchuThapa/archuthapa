afe5f28e966ded17d3ee6a6ae27d65dbb3b18643
ArchuThapa <archanathapa2468@gmail.com>
2024-05-01 09:55:57 +0545

    ChatBot Integrated

--- src/component/Message.js
+++ src/component/Message.js
@@ -15,6 +15,7 @@
 // };
 
 // export default Meassage ;
+
 // import { IonIcon } from "@ionic/react";
 // import { chatbubbleEllipsesOutline } from 'ionicons/icons';
 
@@ -35,7 +36,31 @@
 // export default Message;
 
 
-import { useEffect } from "react";
+// import { useEffect } from "react";
+
+// const Message = () => {
+//   useEffect(() => {
+//     const tidioScript = document.createElement('script');
+//     tidioScript.src = '//code.tidio.co/2vqzvehib3fn0tmrg8m6gtnam2ifkixv.js';
+//     tidioScript.async = true;
+//     document.body.appendChild(tidioScript);
+
+//     return () => {
+//       document.body.removeChild(tidioScript);
+//     };
+//   }, []);
+
+//   return null; // We don't need to render anything here, as Tidio chat widget will be added dynamically
+// };
+
+// export default Message;
+
+
+
+
+import React, { useEffect } from "react";
+import { IonIcon } from "@ionic/react";
+import { chatbubbleEllipsesOutline } from 'ionicons/icons';
 
 const Message = () => {
   useEffect(() => {
@@ -49,7 +74,19 @@ const Message = () => {
     };
   }, []);
 
-  return null; // We don't need to render anything here, as Tidio chat widget will be added dynamically
+  const handleChatIconClick = () => {
+    // Trigger Tidio chat widget to open
+    if (window.tidioChatApi) {
+      window.tidioChatApi.open();
+    }
+  };
+
+  console.log('hiii', handleChatIconClick);
+  return (
+    <div className="fixed bottom-5 sm:right-8 right-4 z-[999] cursor-pointer text-white text-4xl bg-green-600 w-16 h-16 flex items-center justify-center rounded-full animate-bounce" onClick={handleChatIconClick}>
+      <IonIcon icon={chatbubbleEllipsesOutline} />
+    </div>
+  );
 };
 
 export default Message;
