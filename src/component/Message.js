5e2c0a39d0a17a70425c704ed294e6670cf42a0f
ArchuThapa <archanathapa2468@gmail.com>
2024-05-01 10:29:21 +0545

    ChatBot Integrated

--- src/component/Message.js
+++ src/component/Message.js
@@ -43,31 +43,7 @@
 
 
 
-// import { useEffect } from "react";
-
-// const Message = () => {
-//   useEffect(() => {
-//     const tidioScript = document.createElement('script');
-//     tidioScript.src = '//code.tidio.co/2vqzvehib3fn0tmrg8m6gtnam2ifkixv.js';
-//     tidioScript.async = true;
-//     document.body.appendChild(tidioScript);
-
-//     return () => {
-//       document.body.removeChild(tidioScript);
-//     };
-//   }, []);
-
-//   return null; // We don't need to render anything here, as Tidio chat widget will be added dynamically
-// };
-
-// export default Message;
-
-
-
-
-import React, { useEffect } from "react";
-import { IonIcon } from "@ionic/react";
-import { chatbubbleEllipsesOutline } from 'ionicons/icons';
+import { useEffect } from "react";
 
 const Message = () => {
   useEffect(() => {
@@ -81,19 +57,43 @@ const Message = () => {
     };
   }, []);
 
-  const handleChatIconClick = () => {
-    // Trigger Tidio chat widget to open
-    if (window.tidioChatApi) {
-      window.tidioChatApi.open();
-    }
-  };
-
-  console.log('hiii', handleChatIconClick);
-  return (
-    <div className="fixed bottom-5 sm:right-8 right-4 z-[999] cursor-pointer text-white text-4xl bg-green-600 w-16 h-16 flex items-center justify-center rounded-full animate-bounce" onClick={handleChatIconClick}>
-      <IonIcon icon={chatbubbleEllipsesOutline} />
-    </div>
-  );
+  return null; // We don't need to render anything here, as Tidio chat widget will be added dynamically
 };
 
 export default Message;
+
+
+
+
+// import React, { useEffect } from "react";
+// import { IonIcon } from "@ionic/react";
+// import { chatbubbleEllipsesOutline } from 'ionicons/icons';
+
+// const Message = () => {
+//   useEffect(() => {
+//     const tidioScript = document.createElement('script');
+//     tidioScript.src = '//code.tidio.co/2vqzvehib3fn0tmrg8m6gtnam2ifkixv.js';
+//     tidioScript.async = true;
+//     document.body.appendChild(tidioScript);
+
+//     return () => {
+//       document.body.removeChild(tidioScript);
+//     };
+//   }, []);
+
+//   const handleChatIconClick = () => {
+//     // Trigger Tidio chat widget to open
+//     if (window.tidioChatApi) {
+//       window.tidioChatApi.open();
+//     }
+//   };
+
+//   console.log('hiii', handleChatIconClick);
+//   return (
+//     <div className="fixed bottom-5 sm:right-8 right-4 z-[999] cursor-pointer text-white text-4xl bg-green-600 w-16 h-16 flex items-center justify-center rounded-full animate-bounce" onClick={handleChatIconClick}>
+//       <IonIcon icon={chatbubbleEllipsesOutline} />
+//     </div>
+//   );
+// };
+
+// export default Message;
